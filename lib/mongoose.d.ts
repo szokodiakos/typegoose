09fe5f6048e7899c31395f87033b06221218cd8f
Florian Simon <florian@tentwentyfour.lu>
2018-02-06 14:43:20 +0100

    Stop tracking generated files

--- lib/mongoose.d.ts
+++
@@ -1,4 +0,0 @@
-export declare let Mixed: StringConstructor;
-export declare let ObjectId: StringConstructor;
-export declare let mongoose: any;
-export declare function useMongooseImplementation(implementation: any): void;
