3bcfe32048146a1343391197a5849231f67d858a
Dev Hanmari <devtest1mari@gmail.com>
2018-08-24 17:06:09 +0900

    fix test mongo connection failure

--- src/test/utils/mongoConnect.ts
+++ src/test/utils/mongoConnect.ts
@@ -6,7 +6,24 @@ import * as mongoose from 'mongoose';
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
 const connect = () =>
   new Promise((resolve, reject) =>
-    mongoose.connect(`mongodb://localhost:${MONGO_PORT}/typegoosetest`, {},
+    mongoose.connect(
+      `mongodb://localhost:${MONGO_PORT}/typegoosetest`,
+      {
+        server: {
+          socketOptions: {
+            keepAlive: 1,
+            connectTimeoutMS: 30000,
+          },
+        },
+        replset: {
+          socketOptions: {
+            keepAlive: 1,
+            connectTimeoutMS: 30000,
+          },
+        },
+        reconnectTries: 10,
+        reconnectInterval: 1000,
+      },
       (err) => (err ? reject(err) : resolve())),
   );
 
