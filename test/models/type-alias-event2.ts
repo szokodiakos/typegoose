0d363e6550de6e7ad93fbdad58333c600c68dca0
Fabio Formosa <fabio.formosa@dedagroup.it>
2019-08-28 11:00:26 +0200

    removed commented rows

--- test/models/type-alias-event2.ts
+++ test/models/type-alias-event2.ts
@@ -3,12 +3,6 @@ import * as mongoose from 'mongoose';
 import { prop, Typegoose } from '../../src/typegoose';
 
 export class Event extends Typegoose {
-  // constructor(fieldTwo: string, fieldOne?: string) {
-  //   super();
-  //   this.fieldOne = fieldOne;
-  //   this.fieldTwo = fieldTwo;
-  // }
-
   @prop()
   public name: string;
 
